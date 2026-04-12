fd9aef0e7c484e5e981046031295972bb3a5709a
sdomala <domala_s@hotmail.com>
2026-04-11 23:35:06 -0500

    Fixed resource group

--- Terraform-flow/main.tf
+++ Terraform-flow/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "terra" {
 resource "azurerm_virtual_network" "terra" {
     name = var.virtual_network
     location = var.location
-    resource_group_name = azurerm_linux_virtual_machine.terra.name
+    resource_group_name = azurerm_resource_group.terra.name
     address_space = ["10.0.0.0/20"]
   
 }
